e118b49e20add3e7c29bbba04495c7dd560dfdd5
Michal Lata <mikepatch.code@gmail.com>
2023-09-22 20:38:50 +0200

    feat: Add BurgerButton component

---
+++ src/components/Navbar/BurgerButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cn from 'classnames';
+
+import styles from './burgerButton.module.scss';
+
+type BurgerButtonProps = {
+    isOpen: Boolean;
+    toggleMenu: () => void;
+};
+
+export default function BurgerButton({ isOpen, toggleMenu }: BurgerButtonProps) {
+    const buttonStyles = cn(styles.burgerIcon, isOpen && styles.active);
+
+    return (
+        <button className={buttonStyles} onClick={toggleMenu}>
+            <div />
+            <div />
+            <div />
+        </button>
+    );
+}
