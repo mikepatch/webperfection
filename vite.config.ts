ff0c4550b03765016f6a86c11940fd5eafe4ed44
Michal Lata <mikepatch.code@gmail.com>
2023-09-22 20:27:20 +0200

    chore: install svgr dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 import path from 'path';
 
@@ -10,5 +11,5 @@ export default defineConfig({
             '@styles': path.resolve(__dirname, './src/styles'),
         },
     },
-    plugins: [react()],
+    plugins: [react(), svgr()],
 });
