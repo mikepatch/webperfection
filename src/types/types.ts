6e650ea566b5d16e70bb751c190a0b4672952cd3
Michal Lata <michallata.dev@gmail.com>
2023-10-15 20:08:48 +0200

    patch: Extend PropsWithChildren type with className optional prop

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,3 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
-export type PropsWithChildren<P = unknown> = P & { children: ReactNode };
+export type PropsWithChildren<P = unknown> = P & { children: ReactNode; className?: string };
