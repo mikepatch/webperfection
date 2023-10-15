a14be55bcabfa682b20ac4cf926d5084f91b03bb
Michal Lata <michallata.dev@gmail.com>
2023-10-15 20:04:28 +0200

    patch: Update the method of using svg files.

--- src/components/SocialMediaBar/SocialMediaBar.tsx
+++ src/components/SocialMediaBar/SocialMediaBar.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-
-import LinkedInIcon from '../../assets/icons/icon-linkedin.svg?react';
-import FacebookIcon from '../../assets/icons/icon-facebook.svg?react';
-import YoutubeIcon from '../../assets/icons/icon-youtube.svg?react';
 import styles from './socialMediaBar.module.scss';
+import { Icon } from '../common';
 
 type SocialMediaBarProps = {
     color?: string;
@@ -11,7 +7,7 @@ type SocialMediaBarProps = {
 
 type SocialMediaItem = {
     id: string;
-    Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+    iconId: string;
     href: string;
 };
 
@@ -19,27 +15,29 @@ export default function SocialMediaBar({ color = '#000' }: SocialMediaBarProps) 
     const SOCIAL_MEDIA_ITEMS: SocialMediaItem[] = [
         {
             id: 'linkedIn',
-            Icon: LinkedInIcon,
+            iconId: 'icon-linkedin',
             href: 'link',
         },
         {
             id: 'facebook',
-            Icon: FacebookIcon,
+            iconId: 'icon-facebook',
             href: 'link',
         },
         {
             id: 'youtube',
-            Icon: YoutubeIcon,
+            iconId: 'icon-youtube',
             href: 'link',
         },
     ];
 
-    const renderSocialMediaItems = SOCIAL_MEDIA_ITEMS.map(({ id, Icon, href }: SocialMediaItem) => (
-        <li key={id}>
-            <a href={href}>
-                <Icon style={{ fill: color }} />
-            </a>
-        </li>
-    ));
+    const renderSocialMediaItems = SOCIAL_MEDIA_ITEMS.map(
+        ({ id, iconId, href }: SocialMediaItem) => (
+            <li key={id} className={styles.socialMediaItem}>
+                <a href={href}>
+                    <Icon id={iconId} style={{ fill: color }} />
+                </a>
+            </li>
+        )
+    );
     return <ul className={styles.socialMediaList}>{renderSocialMediaItems}</ul>;
 }
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 import Button from './Button';
+import Icon from './Icon';
 
-export default Button;
+export { Button, Icon };
