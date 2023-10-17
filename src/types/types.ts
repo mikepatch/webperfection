177ba118c0efaec9726de18f7cf0e0d07a3a7c52
Michal Lata <michallata.dev@gmail.com>
2023-10-18 00:19:33 +0200

    feat: Add Form component

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,14 @@
 import { ReactNode } from 'react';
 
 export type PropsWithChildren<P = unknown> = P & { children: ReactNode; className?: string };
+
+export type FormField = {
+    id: string;
+    name: string;
+    label: string;
+    placeholder: string;
+    pattern: string;
+    type: 'text' | 'number' | 'email' | 'submit' | 'textarea';
+    required: boolean;
+    errorMessage: string;
+}
