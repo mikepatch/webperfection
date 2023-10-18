c05fb9773384e30ed38e0a6eacec39aa506002df
Michal Lata <michallata.dev@gmail.com>
2023-10-18 14:02:12 +0200

    feat: Add description section

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,3 +1,4 @@
+import Description from '../components/Home/Description';
 import Form from '../components/Home/Form';
 import Hero from '../components/Home/Hero';
 import Offer from '../components/Home/Offer';
@@ -6,9 +7,10 @@ export default function Home() {
     return (
         <>
             <Hero />
-            <main style={{ display: 'flex', flexDirection: 'column', gap: 'var(--space-lg)' }}>
+            <main>
                 <Offer />
                 <Form />
+                <Description />
             </main>
         </>
     );
