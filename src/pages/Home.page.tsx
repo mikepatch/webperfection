c37a99d44a2a92f431dde469fdfd099424a12723
Michal Lata <michallata.dev@gmail.com>
2023-10-22 18:13:06 +0200

    feat: Add Team component

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -2,6 +2,7 @@ import Description from '../components/Home/Description';
 import Form from '../components/Home/Form';
 import Hero from '../components/Home/Hero';
 import Offer from '../components/Home/Offer';
+import Team from '../components/Home/Team';
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
                 <Offer />
                 <Form />
                 <Description />
+                <Team />
             </main>
         </>
     );
