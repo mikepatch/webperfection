2510c382518f02e8bd62b349383e9aba1287f654
Michal Lata <michallata.dev@gmail.com>
2023-10-24 01:05:51 +0200

    feat: Add Link component

--- src/components/common/CTAButton/CTAButton.tsx
+++ src/components/common/CTAButton/CTAButton.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames';
 
 import { PropsWithChildren } from '../../../types';
 import styles from './ctaButton.module.scss';
+import Link from '../Link';
 
 type CTAButtonProps = PropsWithChildren & {
     href: string;
@@ -12,14 +13,14 @@ export default function CTAButton({ href, variant, children, className = '' }: C
     const VARIANTS = {
         bordered: styles.bordered,
     };
-    
+
     return (
-        <a
-            href={href}
+        <Link
+            to={href}
             role="button"
             className={cn(styles.ctaBtn, variant && VARIANTS[variant], className)}
         >
             {children}
-        </a>
+        </Link>
     );
 }
