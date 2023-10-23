be9178ccbe966618f89c622a2fa047fec30159f9
Michal Lata <michallata.dev@gmail.com>
2023-10-23 17:31:05 +0200

    patch: Update Team component to use Slider component

--- src/components/Home/Team/Team.tsx
+++ src/components/Home/Team/Team.tsx
@@ -1,26 +1,19 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
-
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-import 'swiper/scss/scrollbar';
-
 import styles from './team.module.scss';
 import { TEAM_MEMBERS } from '../../../constants';
 import { TeamMember } from '../../../types';
 import BrandLogo from '../../common/BrandLogo';
+import Slider from '../../common/Slider';
 
 export default function Team() {
     const renderSlides = TEAM_MEMBERS.map((teamMember: TeamMember) => (
-        <SwiperSlide key={teamMember.id} className={styles.sliderSlide}>
+        <div key={teamMember.id} className={styles.sliderWrapper}>
             <figure className={styles.sliderFigure}>
                 <img src={teamMember.imgSrc} aria-disabled="true" className={styles.sliderImage} />
                 <figcaption className={styles.sliderCaption}>
                     <h5 className={styles.captionHeading}>{teamMember.role}</h5>
                 </figcaption>
             </figure>
-        </SwiperSlide>
+        </div>
     ));
 
     return (
@@ -29,22 +22,7 @@ export default function Team() {
                 <BrandLogo variant="mini" className={styles.logo} />
                 <h3 className={styles.headerTitle}>Poznaj Twój zespół seniorów</h3>
             </header>
-            <Swiper
-                autoplay={{ delay: 3000 }}
-                centerInsufficientSlides
-                className={styles.slider}
-                grabCursor
-                modules={[Pagination, Autoplay]}
-                pagination={{
-                    clickable: true,
-                    bulletClass: styles.sliderDot,
-                    bulletActiveClass: styles.sliderDotActive,
-                }}
-                slidesPerView="auto"
-                spaceBetween={30}
-            >
-                {renderSlides}
-            </Swiper>
+            <Slider autoplay={{ delay: 3000 }}>{renderSlides}</Slider>
         </section>
     );
 }
