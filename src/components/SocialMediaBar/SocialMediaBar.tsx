2b6f4cb3fd55db4d2cb5d1a94dd3f4abcf214b9d
Michal Lata <michallata.dev@gmail.com>
2023-11-13 17:09:29 +0100

    patch: Move types and constants from SocialMediaBar component

--- src/components/SocialMediaBar/SocialMediaBar.tsx
+++ src/components/SocialMediaBar/SocialMediaBar.tsx
@@ -1,36 +1,9 @@
+import { SocialMediaBarProps, SocialMediaItem } from '@/types';
+import { SOCIAL_MEDIA_ITEMS } from '@/constants';
 import Icon from '../common/Icon';
 import styles from './socialMediaBar.module.scss';
 
-
-type SocialMediaBarProps = {
-    color?: string;
-};
-
-type SocialMediaItem = {
-    id: string;
-    iconId: string;
-    href: string;
-};
-
 export default function SocialMediaBar({ color = '#000' }: SocialMediaBarProps) {
-    const SOCIAL_MEDIA_ITEMS: SocialMediaItem[] = [
-        {
-            id: 'linkedIn',
-            iconId: 'icon-linkedin',
-            href: 'link',
-        },
-        {
-            id: 'facebook',
-            iconId: 'icon-facebook',
-            href: 'link',
-        },
-        {
-            id: 'youtube',
-            iconId: 'icon-youtube',
-            href: 'link',
-        },
-    ];
-
     const renderSocialMediaItems = SOCIAL_MEDIA_ITEMS.map(
         ({ id, iconId, href }: SocialMediaItem) => (
             <li key={id} className={styles.socialMediaItem}>
