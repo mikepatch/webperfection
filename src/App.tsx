dd0468803f003d432b3788e274c14d961e840ea1
Michal Lata <michallata.dev@gmail.com>
2023-11-13 17:14:38 +0100

    patch: Update file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './styles/main.scss';
 
-import ContactForm from './components/ContactForm';
 import Description from './components/Description';
 import Layout from './components/Layout';
 import Offer from './components/Offer';
 import Summary from './components/Summary';
 import Team from './components/Team';
 import Testimonials from './components/Testimonials';
+import ContactForm from './components/ContactForm';
 
 function App() {
     return (
